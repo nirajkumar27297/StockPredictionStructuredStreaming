9671e5feefbc5e0823cc33be1074bb072549bc37
nirajkumar27297 <nirajkumar17315@gmail.com>
2020-11-23 18:05:11 +0530

    [Niraj]Added:Docker File for Spark Installation

---
+++ DockerFiles/DockerScript.sh
@@ -0,0 +1,2 @@
+cd StockPredictionStructuredStreaming && sbt assembly && spark-submit --driver-java-options "-Dlog4j.configuration=log4j.properties" --packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.0.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0 --class SparkStructuredStreaming.StockPredictionKafkaStructuredStreaming "./target/scala-2.12/StockPredictionStructuredStreaming-assembly-0.1.jar" "127.0.0.1:9092" "kafkatutorial" "./SavedOutput" "./pythonFiles/StockPricePrediction.py"
+
--- build.sbt
+++ build.sbt
@@ -15,8 +15,3 @@ libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka-0-10" % "2.4
 
 // https://mvnrepository.com/artifact/org.apache.spark/spark-sql-kafka-0-10
 libraryDependencies += "org.apache.spark" %% "spark-sql-kafka-0-10" % "3.0.0"
-
-assemblyMergeStrategy in assembly := {
-  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
-  case x                             => MergeStrategy.first
-}
